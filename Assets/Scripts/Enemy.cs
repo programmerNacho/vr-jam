303e385f6fa60c5feeed311403ca37a231dfc13c
Ignacio <ignacio.programmer@gmail.com>
2020-12-11 03:00:30 +0100

    Basic Enemy and Nexus interaction. Reload scene on lose

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float attackRange = 3f;
+    [SerializeField]
+    private float attackSpeed = 1f;
+
+    private NavMeshAgent navMeshAgent = null;
+    private Damager damager = null;
+
+    private Nexus nexus = null;
+    private Health nexusHealth = null;
+
+    private float timeRemainingForAttack = 0f;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        damager = GetComponent<Damager>();
+
+        nexus = FindObjectOfType<Nexus>();
+        if(nexus)
+        {
+            nexusHealth = nexus.GetComponent<Health>();
+        }
+    }
+
+    private void Start()
+    {
+        SetDestinationToNexus();
+    }
+
+    private void SetDestinationToNexus()
+    {
+        navMeshAgent.SetDestination(nexus.transform.position);
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, nexus.transform.position) <= attackRange)
+        {
+            navMeshAgent.velocity = Vector3.zero;
+            navMeshAgent.SetDestination(transform.position);
+
+            AttackNexus();
+        }
+        else
+        {
+            timeRemainingForAttack = attackSpeed;
+        }
+    }
+
+    private void AttackNexus()
+    {
+        if(nexusHealth.IsAlive)
+        {
+            timeRemainingForAttack -= Time.deltaTime;
+
+            if (timeRemainingForAttack <= 0f)
+            {
+                timeRemainingForAttack = attackSpeed;
+                damager.Attack(nexusHealth);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Nexus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Nexus : MonoBehaviour
+{
+    private Health health = null;
+
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+    }
+
+    private void OnEnable()
+    {
+        health.OnDeath.AddListener(Dead);
+    }
+
+    private void OnDisable()
+    {
+        health.OnDeath.RemoveListener(Dead);
+    }
+
+    private void Dead()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
